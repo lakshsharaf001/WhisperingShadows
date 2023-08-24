e020b3549b7717ded05890baf5c8bcded2ada894
benongg <117260589+benongg@users.noreply.github.com>
2023-08-24 12:13:15 +0800

    Updates
    Updated Keys and book to make sure it can be collected and once done , mission success scene will display

--- Assets/Scripts/Collect Items/PlayerInventory.cs
+++ Assets/Scripts/Collect Items/PlayerInventory.cs
@@ -1,28 +1,45 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class PlayerInventory : MonoBehaviour
 {
-    public int NumberOfKeys {get; private set;}
-
-    public int NumberOfBooks {get; private set;}
-
+    public int NumberOfKeys { get; private set; }
+    public int NumberOfBooks { get; private set; }
+    
     public UnityEvent<PlayerInventory> OnKeyCollected;
     public UnityEvent<PlayerInventory> OnBookCollected;
+    
+
+    private void CheckMissionStatus()
+    {
+        if (NumberOfKeys >= 4 && NumberOfBooks >= 4)
+        {
+            MissionSuccess();
+        }
+    }
+
+    private void MissionSuccess()
+    {
+        // Load the missionsuccess scene
+        SceneManager.LoadScene("MissionSuccess"); // Replace with your scene's name
+    }
 
     public void KeyCollected()
     {
         NumberOfKeys++;
         OnKeyCollected.Invoke(this);
+        CheckMissionStatus();
     }
 
     public void BookCollected()
     {
         NumberOfBooks++;
         OnBookCollected.Invoke(this);
+        CheckMissionStatus();
     }
-
 
 }
